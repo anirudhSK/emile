b2d07b064e3f2bc924715cb54cc28ef487b2ad4b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-12 16:09:58 -0700

    Cosmetic changes. Try and integrate with Remy now.

--- emile_worker.py
+++ emile_worker.py
@@ -16,15 +16,19 @@ while (True):
        print "Running the problem we found\n";
        print "Sleep time ",str(random() * 5.0)
        args = [ '/bin/sleep', str( random() * 5.0 ) ]
+       # TODO: args = [ '/remy/', 'if=...', 'of=...', ]
        process_handles.append( ( subprocess.Popen( args ), reply.text, reply.headers[ 'problem_id' ] ) )
 
   # check all handles
   remove_handles = []
   for i in range( 0, len( process_handles ) ):
     if (process_handles[i][0].poll() is not None):
-      print "Process return code", process_handles[i][0].returncode
+      return_code  = process_handles[i][0].returncode
       remove_handles.append( process_handles[ i ] )
-      post_status = requests.post( 'http://localhost:5000/answer', data = { 'problem_id' : process_handles[i][2], 'answer' : process_handles[i][1] } )
+      post_status = requests.post( 'http://localhost:5000/answer',
+                                    data = { 'problem_id' : process_handles[i][2],
+                                             'answer' : process_handles[i][1],
+                                             'return_code' : return_code } )
 
   # reap the ones that are done
   for handle in remove_handles:
