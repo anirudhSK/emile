f85164e5a4210e10f93f2e14d0eee87ecc3ae318
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-12 13:58:01 -0700

    Intermediate commit before switching to subprocess (instead of multiprocessing)

--- emile_worker.py
+++ emile_worker.py
@@ -1,19 +1,29 @@
 import requests
 import multiprocessing
 from time import sleep
+from random import random
+import logging
 
 def myfunction(problem_id, problem):
-  sleep(5)
-  # TODO: In general, you would do much more than just echo
-  return (problem_id, problem)
+##  try:
+    sleep_time = random() * 5.0
+    logging.info("Sleep time computed\n");
+    sleep( sleep_time )
+    print "Hello"
+    # TODO: In general, you would do much more than just echo
+    return (problem_id, problem)
+##  except:
+##    logging.critical("Errored out here\n");
 
 def post_answers(problem_tuple):
   reply = requests.post( 'http://localhost:5000/answer', data = { 'problem_id' : problem_tuple[0], 'answer' : problem_tuple[1] } )
+  logging.info("OK");
   print reply.text 
 
-pool = multiprocessing.Pool(processes = 30)
+pool = multiprocessing.Pool(processes = 40)
 while (True):
   reply = requests.get( 'http://localhost:5000/question' )
+  print "Current pool cache size is", len(pool._cache)
   if (reply.status_code == 404):
     print "No problems, sleeping 1 second before trying again"
     sleep(1)
--- optimizer_dummy.py
+++ optimizer_dummy.py
@@ -1,13 +1,13 @@
 import requests
 import random
 # TODO: Replicate this using libcurl
+NUM_TASKS = 50
+problem_id = [''] * NUM_TASKS
 
-problem_id = [''] * 100
-
-for i in range(0, 100):
+for i in range(0, NUM_TASKS):
   reply = requests.post( 'http://localhost:5000/problem', data = 'deadbeef' + str(random.random()) )
   problem_id[i] = reply.text
 
-for i in range(0, 100):
+for i in range(0, NUM_TASKS):
   reply = requests.get( 'http://localhost:5000/problem', data = problem_id[i] )
   print reply.text
