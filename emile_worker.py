0f0924757eebfbb60c02a5a76811dc847b6da286
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-13 11:05:16 -0700

    Use temporary and unique file names for running processes.

--- emile_worker.py
+++ emile_worker.py
@@ -2,10 +2,11 @@ import requests
 import httplib
 import subprocess
 import os
+import tempfile
 from time import sleep
+
 process_handles = []
-remy_binary  = '/home/anirudh/remy/src/rat-runner'
-answer_file =  '/home/anirudh/remy/src/test.answer'
+remy_binary  = '/usr/local/google/home/anirudhsk/remy/src/rat-runner'
 while (True):
   if (len(process_handles) == 0):
     sleep( 1 )
@@ -19,30 +20,39 @@ while (True):
     reply = http_get.getresponse()
     if ( reply.status == 200 ):
        print "Running the problem we found\n";
-       open( "/tmp/problem", "wb").write( reply.read() )
-       args = [ remy_binary, 'problem=/tmp/problem', 'answer=' + str(answer_file) ]
+       problem_fd = tempfile.NamedTemporaryFile()
+       answer_fd  = tempfile.NamedTemporaryFile()
+       problem_fd.write( reply.read() )
+       problem_fd.flush()
+       args = [ remy_binary, 'problem=' + problem_fd.name, 'answer=' + answer_fd.name ]
        fnull = open( os.devnull, 'w' )
-       process_handles.append( ( subprocess.Popen( args,
-                                                   stdout = fnull,
-                                                   stderr = subprocess.STDOUT  ),
-                                 reply.getheader( 'problem_id' )
-                               ),
+       process_handles.append( { 'process' : subprocess.Popen( args,
+                                                               stdout = fnull,
+                                                               stderr = subprocess.STDOUT  ),
+                                 'id'      : reply.getheader( 'problem_id' ),
+                                 'problem' : problem_fd,
+                                 'answer'  : answer_fd
+                               },
                              )
 
   # check all handles
   remove_handles = []
   for i in range( 0, len( process_handles ) ):
-    if (process_handles[i][0].poll() is not None):
-      return_code  = process_handles[i][0].returncode
-      remove_handles.append( process_handles[ i ] )
+    if (process_handles[i]['process'].poll() is not None):
+      return_code  = process_handles[i]['process'].returncode
       http_post = httplib.HTTPConnection( 'localhost:5000' )
+      process_handles[i]['answer'].flush();
       http_post.request( 'POST', '/answer',
-                         body = open( answer_file, 'rb' ).read(),
-                         headers = { 'problem_id' : process_handles[i][1],
+                         body = process_handles[i]['answer'].read(),
+                         headers = { 'problem_id' : process_handles[i]['id'],
                                      'return_code': return_code } )
       post_status = http_post.getresponse()
       assert( post_status.status == 200 );
       assert( post_status.read() == "OK" );
+      # Close tmp files and reap dead processes
+      process_handles[i]['problem'].close();
+      process_handles[i]['answer'].close();
+      remove_handles.append( process_handles[ i ] )
 
   # reap the ones that are done
   for handle in remove_handles:
