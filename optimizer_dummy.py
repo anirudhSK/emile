c93b07fc3ac9c80dbaaf526e6e241fd680ad7c8b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-13 02:59:02 -0400

    Pretty print out result

--- optimizer_dummy.py
+++ optimizer_dummy.py
@@ -2,7 +2,7 @@ import requests
 import random
 import answer_pb2
 # TODO: Replicate this using libcurl
-NUM_TASKS = 1
+NUM_TASKS = 50
 problem_id = [''] * NUM_TASKS
 
 for i in range(0, NUM_TASKS):
@@ -13,4 +13,5 @@ for i in range(0, NUM_TASKS):
   reply = requests.get( 'http://localhost:5000/problem', data = problem_id[i] )
   answer_proto = answer_pb2.Outcome()
   open("/tmp/interim", "wb").write( reply.content );
-  answer_proto.ParseFromString( reply.content )#/usr/local/google/home/anirudhsk/remy/src/test.answer )
+  answer_proto.ParseFromString( reply.content )
+  print str( answer_proto )
