2bff65933d5949db313937f7045ef21baa26561b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-20 19:49:44 -0700

    Clean up scripts, and make sure it actually works.

--- emile.pp
+++ emile.pp
@@ -4,7 +4,10 @@ exec { "apt-get update":
       path => $sys_path,
 }
 
-$basic_packages = ['apache2', 'python-pip', 'libcurl4-openssl-dev', 'libboost-all-dev', 'libprotobuf-dev', 'protobuf-compiler', 'git', 'libapache2-mod-wsgi', 'redis-server', 'autoconf', 'build-essential', 'libtool', 'pkg-config', 'htop' ]
+$basic_packages = ['apache2', 'python-pip', 'libcurl4-openssl-dev',
+                   'libboost-all-dev', 'libprotobuf-dev', 'protobuf-compiler',
+                   'git', 'libapache2-mod-wsgi', 'redis-server',
+                   'autoconf', 'build-essential', 'libtool', 'pkg-config', 'htop' ]
 
 package { $basic_packages:
       ensure  => present,
@@ -26,65 +29,62 @@ exec {"pip install protobuf":
       require => Package[$basic_packages],
 }
 
-# Start redis only after changing file
-service { "redis-server",
-      ensure => "stopped"
+# REDIS
+# Ensure redis is running
+service { "redis-server":
+      ensure => "running",
+      require => Package["redis-server"],
 }
 
-# Copy over redis conf
+# Copy over redis conf to enable UNIX sockets
 file { '/etc/redis/redis.conf':
       content => template('redis.templ'),
-      notify  => Service["redis-server"]
-}
-
-# Start redis only if we have the files setup
-service { "redis-server",
-      ensure => "running",
-      enable => "true", 
+      notify  => Service["redis-server"],
       require => Package["redis-server"]
 }
 
+# APACHE
 # Ensure apache is running
-service { "apache",
+service { "apache2":
       ensure => "running",
-      enable => "true",
+      require => Package["apache2"],
+}
+
+# Increase timeout for Apache keep-alive messages to 1000 seconds
+file { '/etc/apache2/apache2.conf':
+      content => template('apache2.templ'),
+      notify  => Service["apache2"],
       require => Package["apache2"]
 }
 
-# Apache keep-alive messages
-file { '/etc/apache2/apache2.conf',
-      content => template('apache2.templ'),
-      notify  => Service["apache"]
-}
-
-# sites-enabled for virtual host
-file { '/etc/apache2/sites-enabled/emile_master.conf',
-      notify => Service["apache"]
-      content => template('emile_master.conf')
-      require => [ File["/var/www/emile_master/emile_master.py"],
-                   File["/var/www/emile_master/emile_master.py"] ]
-}
-
-# check out emile to a normal folder and change DocumentRoot appropriately.
-file { '/var/www/emile_master',
+# copy emile scripts to /var/www
+file { '/var/www/emile_master':
       ensure => "directory",
       owner  => "www-data",
       group  => "root",
-      require => Package["apache2"]
+      require => Package["apache2"],
 }
 
-file { '/var/www/emile_master/emile_master.py',
+file { '/var/www/emile_master/emile_master.py':
       content => template('emile_master.py'),
       owner  => "www-data",
       group  => "root",
-      require => File["/var/www/emile_master"]
+      require => File["/var/www/emile_master"],
 }
 
-file { '/var/www/emile_master/emile_master.wsgi',
+file { '/var/www/emile_master/emile_master.wsgi':
       content => template('emile_master.wsgi'),
       owner  => "www-data",
       group  => "root",
-      require => File["/var/www/emile_master"]
+      require => File["/var/www/emile_master"],
 }
 
-# SIGUSR1 errors
+# sites-enabled for virtual host
+file { '/etc/apache2/sites-enabled/emile_master.conf':
+      notify => Service["apache2"],
+      content => template('emile_master.conf'),
+      require => [ File["/var/www/emile_master/emile_master.py"],
+                   File["/var/www/emile_master/emile_master.wsgi"] ],
+}
+
+# TODO: SIGUSR1 errors
