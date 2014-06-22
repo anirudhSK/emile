14b356383e59f1a403ab516f54158d92de202bea
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-22 04:39:44 +0000

    Add startup script to set permissions (hopefully fixes #5) and cleanly flush once.

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -x
+sudo service apache2 stop
+redis-cli flushall
+sudo cp emile_master.py /var/www/emile_master
+sudo cp emile_master.wsgi /var/www/emile_master
+sudo chown www-data /var/www/emile_master/emile_master.py
+sudo chown www-data /var/www/emile_master/emile_master.wsgi
+sudo service redis-server restart
+sudo chmod 777 /var/run/redis/redis.sock
+sudo service apache2 start
